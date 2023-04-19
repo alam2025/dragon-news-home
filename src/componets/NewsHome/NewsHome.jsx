d2eca3ab0cb55d6d882f62747deab449b3df5bce
alam2025 <alam13100852@gmail.com>
2023-04-19 22:26:04 +0600

    dd

--- src/componets/NewsHome/NewsHome.jsx
+++ src/componets/NewsHome/NewsHome.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NewsHome = () => {
       return (
             <div>
-                 <h5 className='fs-4 mb-3'>Dragon News Home</h5>
+                 <h5 className='fs-4   mb-3'>Dragon News Home</h5>
             </div>
       );
 };
